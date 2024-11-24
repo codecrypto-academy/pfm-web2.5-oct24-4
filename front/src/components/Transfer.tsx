0a8e223e75426001f04f69642a5dcb387ca32ebc
jsecchi88 <91900285+jsecchi88@users.noreply.github.com>
2024-11-24 14:57:34 -0300

    Shadcn

--- front/src/components/Transfer.tsx
+++ front/src/components/Transfer.tsx
@@ -1,3 +1,13 @@
+import useForm from 'react-hook-form';
+import { Form, FormItem } from '.form';
+
 export function Transfer () {
-  return <div>Transfer</div>;
+  const form = useForm
+
+  return <div className="space-y-4 mt-3">
+    
+    <h1 className="text-xl font-bold">Transfer</h1>
+    <p>Transfer tokens between accounts</p>
+
+  </div>;
 }
