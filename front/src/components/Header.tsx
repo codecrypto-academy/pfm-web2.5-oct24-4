0139dacc322b9ec04f87a837de07c94240035cd2
CHV <andreschv@gmail.com>
2024-11-26 19:29:56 +0100

    Added links to faucet and return home

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaEthereum } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
@@ -11,8 +12,12 @@ const Header: React.FC = () => {
         </div>
         <nav>
           <ul className="flex space-x-6">
+            <Link to="/">
             <li><a href="#home" className="hover:text-yellow-300">Home</a></li>
+            </Link>
+            <Link to="/faucet">
             <li><a href="#faucet" className="hover:text-yellow-300">Faucet</a></li>
+            </Link>
             <li><a href="#about" className="hover:text-yellow-300">About</a></li>
             <li><a href="#contact" className="hover:text-yellow-300">Contact</a></li>
           </ul>
